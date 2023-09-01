4c64e953f22d2271c88a0f86f0d11915584d6e65
Gleb Korotky <korotkiygs@mail.ru>
2023-09-01 13:17:29 +0300

    migratoins add

---
+++ server/migrations/versions/20d530ea6d1d_initialdb.py
@@ -0,0 +1,24 @@
+"""InitialDB
+
+Revision ID: 20d530ea6d1d
+Revises: 
+Create Date: 2023-09-01 13:15:57.649491
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '20d530ea6d1d'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
